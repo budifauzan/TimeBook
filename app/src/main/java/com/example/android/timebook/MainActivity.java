52e97b27c732e93bc3b04bbe0ac42e8b0bb7f08b
Muhammad Budi fauzan <budifauzan1997@gmail.com>
2018-05-05 19:27:08 +0700

    Fixing Clear OnClick EditText

--- app/src/main/java/com/example/android/timebook/MainActivity.java
+++ app/src/main/java/com/example/android/timebook/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.timebook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent myIntent = new Intent(MainActivity.this, TimelineActivity.class);
+        Intent myIntent = new Intent(MainActivity.this, TambahJadwalActivity.class);
         MainActivity.this.startActivity(myIntent);
+
+
     }
 
 }
--- app/src/main/java/com/example/android/timebook/TambahJadwalActivity.java
+++ app/src/main/java/com/example/android/timebook/TambahJadwalActivity.java
@@ -1,13 +1,31 @@
 package com.example.android.timebook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
-public class TambahJadwalActivity extends AppCompatActivity {
+public class TambahJadwalActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editText[] = new EditText[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tambah_jadwal_activity);
+        editText[0] = findViewById(R.id.editNamaKegiatan);
+        editText[1] = findViewById(R.id.editLokasiKegiatan);
+        editText[1].setOnClickListener(this);
+        editText[0].setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == editText[0].getId()) {
+            editText[0].setText("");
+        } else if (v.getId() == editText[1].getId()) {
+            editText[1].setText("");
+        }
+    }
+
 }
