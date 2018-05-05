a421fad6fee407bf3604ccb70d353fc8acb5721a
Muhammad Budi fauzan <budifauzan1997@gmail.com>
2018-05-05 08:32:40 +0700

    Timeline Activity
    Membuat timeline activity

--- app/src/main/java/com/example/android/timebook/TimelineActivity.java
+++ app/src/main/java/com/example/android/timebook/TimelineActivity.java
@@ -3,11 +3,11 @@ package com.example.android.timebook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TambahJadwalActivity extends AppCompatActivity {
+public class TimelineActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tambah_jadwal_activity);
+        setContentView(R.layout.activity_timeline);
     }
 }
